c279cb5b2b66258f61bf330458235eae46ffb40e
MiguelRamosAA <Miguel.Ramos2@aa.com>
2023-04-10 13:31:41 -0600

    Add spring boot configuration

---
+++ JavaStreetAPI/src/main/java/com/javastreet/JavaStreetAPI/JavaStreetApiApplication.java
@@ -0,0 +1,13 @@
+package com.javastreet.JavaStreetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStreetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStreetApiApplication.class, args);
+	}
+
+}
---
+++ JavaStreetAPI/src/test/java/com/javastreet/JavaStreetAPI/JavaStreetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastreet.JavaStreetAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStreetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
